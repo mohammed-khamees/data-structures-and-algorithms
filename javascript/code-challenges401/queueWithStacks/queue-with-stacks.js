12dc52ea7314e3d883345d2880436fe4245b60e2
mohammed-khamees <mohammed.khamis1996@yahoo.com>
2021-04-12 16:40:10 +0300

    with stacks

--- javascript/code-challenges401/queueWithStacks/queue-with-stacks.js
+++ javascript/code-challenges401/queueWithStacks/queue-with-stacks.js
@@ -1,23 +1,25 @@
 "use strict";
 
+const { Stack } = require("./../stacksAndQueues/stacks-and-queues");
+
 class PseudoQueue {
   constructor() {
-    this.stack1 = [];
-    this.stack2 = [];
+    this.stack1 = new Stack();
+    this.stack2 = new Stack();
     this.front = null;
   }
 
   enqueue(value) {
-    this.stack1.push(value);
-    this.front = this.stack1[0];
+    this.stack1.items.push(value);
+    this.front = this.stack1.items[0];
   }
 
   dequeue() {
-    while (this.stack1.length > 0) {
-      this.stack2.push(this.stack1.pop());
+    while (this.stack1.items.length > 0) {
+      this.stack2.push(this.stack1.items.pop());
     }
-    let front = this.stack2.pop();
-    this.front = this.stack2[this.stack2.length - 1];
+    let front = this.stack2.items.pop();
+    this.front = this.stack2.items[this.stack2.items.length - 1];
     return front;
   }
 }
