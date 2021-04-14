477670370d962f875bb018c88a47fcd2b5ceac68
mohammed-khamees <mohammed.khamis1996@yahoo.com>
2021-04-14 18:39:09 +0300

    fixed

--- javascript/code-challenges401/multiBracketValidation /multi-bracket-validation.js
+++ javascript/code-challenges401/multiBracketValidation /multi-bracket-validation.js
@@ -4,29 +4,31 @@ const { Stack } = require("./../stacksAndQueues/stacks-and-queues");
 
 function multiBracketValidation(input) {
   let stack1 = new Stack();
-  let stack2 = new Stack();
-  let stack3 = new Stack();
+
   if (typeof input === "string") {
     let inputArr = input.split("");
 
     for (let i = 0; i < inputArr.length; i++) {
-      if (inputArr[i] === "{" || inputArr[i] === "}") {
+      if (inputArr[i] === "{" || inputArr[i] === "[" || inputArr[i] === "(") {
         stack1.push(inputArr[i]);
-      } else if (inputArr[i] === "[" || inputArr[i] === "]") {
-        stack2.push(inputArr[i]);
-      } else if (inputArr[i] === "(" || inputArr[i] === ")") {
-        stack3.push(inputArr[i]);
+      } else {
+        if (inputArr[i] === "}" || inputArr[i] === ")" || inputArr[i] === "]") {
+          let theBracket = 0;
+          if (inputArr[i] === "}") theBracket = "{";
+          if (inputArr[i] === "]") theBracket = "[";
+          if (inputArr[i] === ")") theBracket = "(";
+
+          if (theBracket === stack1.peek()) stack1.pop();
+          else return false;
+        }
       }
     }
 
-    if (
-      stack1.items.length % 2 !== 0 ||
-      stack2.items.length % 2 !== 0 ||
-      stack3.items.length % 2 !== 0
-    ) {
+    if (stack1.top) {
       return false;
+    } else {
+      return true;
     }
-    return true;
   }
 
   return;
